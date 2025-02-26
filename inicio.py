6f5d06d3cb462dff701af5410897d3efbffee617
Leandro Fernandes <lfernandes1898@gmail.com>
2025-02-26 12:03:01 -0300

    git add

--- inicio.py
+++ inicio.py
@@ -15,12 +15,12 @@ print(tabela_vendas)
 
 print ('-' *50)
 #Faturamento por loja
-faturamento=tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum() 
+faturamento=tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum().reset_index() 
 
 print(faturamento)
 #Quantidade de produtos vendidos por loja
 
-quantidade=tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum() 
+quantidade=tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum().reset_index() 
 
 print(quantidade)
 
@@ -31,6 +31,8 @@ print ('-' *50)
 
 ticket_medio =(faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
 ticket_medio=ticket_medio.rename(columns={0: 'Ticket Médio'}) 
+# Unindo o faturamento e a quantidade para incluir ID na tabela do ticket médio
+ticket_medio = faturamento[['ID Loja']].merge(ticket_medio, left_index=True, right_index=True)
 
 print(ticket_medio)
 
@@ -45,13 +47,13 @@ mail.HTMLBody = f'''
 <p>Segue o Relatório de Vendas de cada loja.</p>
 
 <p><b>Faturamento:</b></p>
-{faturamento.to_html(formatters={'Valor Final':'R${:,.2f}'.format})} 
+{faturamento.to_html(formatters={'Valor Final': 'R${:,.2f}'.format}, index=False)} 
 
 <p><b>Quantidade Vendida:</b></p>
-{quantidade.to_html()}
+{quantidade.to_html(index=False)}
 
 <p><b>Ticket médio dos Produtos em cada loja:</b></p>
-{ticket_medio.to_html(formatters={'Ticket Médio':'R${:,.2f}'.format})}
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format}, index=False)}
 
 <p>Qualquer dúvida entre em contato.</p>
 
