9dfa949c4ddc2e71aff2652c11e8fce498895bcf
Leandro Fernandes <lfernandes1898@gmail.com>
2025-02-26 11:01:11 -0300

    Salvando mudanças locais antes do pull

--- inicio.py
+++ inicio.py
@@ -3,36 +3,34 @@ import win32com.client as win32
 
 
 
-#Improtar a base de dados
+#Importar a base de dados
 
 tabela_vendas = pd.read_excel('Vendas.xlsx')
 
 
-
-
 #Visulizar a base de dados
 
-pd.set_option('display.max_columns',None) # serve para mostrar todas as colunas
+pd.set_option('display.max_columns',None) 
 print(tabela_vendas)
 
 print ('-' *50)
 #Faturamento por loja
-faturamento=tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum() #filtra colunas IdLoja valor final e agrupa por Id loja somando os valores de Valor Final
+faturamento=tabela_vendas[['ID Loja','Valor Final']].groupby('ID Loja').sum() 
 
 print(faturamento)
 #Quantidade de produtos vendidos por loja
 
-quantidade=tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum() #filtra colunas IdLoja valor final e agrupa por Id loja somando os valores de Valor Final
+quantidade=tabela_vendas[['ID Loja','Quantidade']].groupby('ID Loja').sum() 
 
 print(quantidade)
 
 #ticket médio por produto em cada loja.
-#Faturamento da loja / quantiade de produto vendido pela loja 
+#Faturamento da loja / quantidade de produto vendido pela loja 
 
 print ('-' *50)
-# ticket= faturamento/quantidade  não posso fazer assim
+
 ticket_medio =(faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
-ticket_medio=ticket_medio.rename(columns={0: 'Ticket Médio'}) #mudou o nome da coluna 0 pata Ticket médio
+ticket_medio=ticket_medio.rename(columns={0: 'Ticket Médio'}) 
 
 print(ticket_medio)
 
@@ -61,8 +59,7 @@ mail.HTMLBody = f'''
 <p>Leandro</p>
 '''
 
-#formata a tabela Valor final  : toda formatação 
-#começa com : , para seprar milhar . para separar decimal 2f é para 2 caas decimal 
+
 mail.Send()
 print('Email enviado')
 
@@ -70,6 +67,5 @@ print('Email enviado')
 
 
 
-#openpyxl pacote para ler excel  py -m pip install openpyxl
 
-#pywmin32 para mandar email
+
